fc4e5b36fd905823e4d830f8f337c5019a53b2e2
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-12-07 20:16:46 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-	lintOnSave: true,
-	devServer: {
-		proxy: {
-			'/base': {
-				target:"http://47.108.165.207/api/",
-				changeOrigin: true,
-				pathRewrite: {
-					'^/base': '' //规定请求地址以什么作为开头
-				}
-			},
-		},
-	},
-}
+  lintOnSave: true,
+  devServer: {
+    disableHostCheck: true,
+    host: "0.0.0.0",
+    port: 8181,
+    proxy: {
+      "/base": {
+        target: "http://47.108.165.207/api/",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/base": "", //规定请求地址以什么作为开头
+        },
+      },
+    },
+  },
+};
