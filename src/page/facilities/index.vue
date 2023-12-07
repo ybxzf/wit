138082221b2980d24ee64d6cc102ddcdcf984342
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-12-07 20:18:27 +0800

    Update index.vue

--- src/page/facilities/index.vue
+++ src/page/facilities/index.vue
@@ -1,53 +1,98 @@
 <template>
-    <div class="indexBox">
-        aaa
-        <!-- <img src="@/assets/img/demo.png"> -->
-        <div class="unusual">
-            <p class="title">异常监测</p>
-            <ul class="swiperHead row">
-                <li>异常类型</li>
-                <li>实验室</li>
-                <li>发生时间</li>
-            </ul>
-            <div class="swiper" id="unusualSwiper">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide" v-for="item in unusual">
-                        <ul class="swiperBody row">
-                            <li><span>{{ item.sensorType }}</span></li>
-                            <li><span>{{ item.labName }}</span></li>
-                            <li><span>{{ item.updateTime }}</span></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
+  <div class="indexBox">
+    <el-row>
+      <el-col :span="6">
+        <el-row>
+          <el-col :span="24">
+            <error-watch :deviceList="this.deviceList"></error-watch>
+          </el-col>
+          <el-col :span="24">
+            <tester :testerList="this.testerList"></tester>
+          </el-col>
+          <el-col :span="24">
+            <env-condition :envList="this.envList"></env-condition>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="18">
+        <device-status :envList="this.envList"></device-status>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import errorWatch from "./components/errorWatch.vue";
+import tester from "./components/tester.vue";
+import envCondition from "./components/envCondition.vue";
+import deviceStatus from "./components/deviceStatus.vue";
 export default {
-    name: 'facilities',
-    data() {
-        return {
-
-        }
+  components: { errorWatch, tester, envCondition, deviceStatus }, //注册组件
+  name: "facilities",
+  data() {
+    return {
+      deviceList: [
+        { name: "设备名称", value: "XXXXX" },
+        { name: "设备编号", value: "XXXXX" },
+        { name: "设备厂家信息", value: "XXXXX" },
+        { name: "所属实验室", value: "XXXXX" },
+        { name: "设备运行状态", value: "正常" },
+      ],
+      testerList: [
+        { name: "张三" },
+        { name: "李四" },
+        { name: "王五" },
+        { name: "赵六" },
+        { name: "赵六" },
+        { name: "赵六" },
+      ],
+      envList: {
+        temperature: "50",
+        humidity: "20",
+        illumination: "100",
+        magneticField: "210",
+      },
+    };
+  },
+  methods: {
+    clickBack() {
+      // this.$router.push("/")
     },
-    methods: {
-        clickBack() {
-            this.$router.push("/")
-        }
-    }
-}
+  },
+};
 </script>
-
+<style scoped>
+@import url("../../assets/css/index.css");
+</style>
 <style scoped>
 .indexBox {
-    width: 100%;
-    min-height: 100%;
-    background-image: url("../../assets/img/bg.png");
-    background-size: cover;
-    background-position: center;
-    position: relative;
+  padding: 10px;
 }
+::-webkit-scrollbar {
+  display: none;
+}
+/* 
+.unusual {
+  width: 100%;
+  height: 24rem;
+  background-image: url("../../assets/img/frame_0.png");
+  background-size: 100% 100%;
+}
+.title {
+  font-size: 1.72rem;
+  text-shadow: 0px 3px 0px rgba(0, 9, 17, 0.52);
+  color: #fff;
+  padding: 0 0 1rem 4rem;
+  font-family: "YS";
+  letter-spacing: 0.15rem;
+  line-height: 2.58rem;
+}
+.indexBox .unusual .swiperHead {
+  width: 28.42rem;
+  margin: 0 auto;
+  height: 3.58rem;
+  font-size: 1.28rem;
+  color: #4bf8fe;
+  line-height: 3.58rem;
+} */
 </style>
