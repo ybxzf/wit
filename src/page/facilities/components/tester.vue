10456f6ffb0f1ee74eb74abac7750cbb12c8421c
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-12-07 20:20:29 +0800

    Create tester.vue

---
+++ src/page/facilities/components/tester.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="unusual">
+    <p class="title">检测人员</p>
+    <ul class="swiperHead row" style="">
+      <ol v-for="(item, index) in testerList">
+        <img class="icon-avatar" src="@/assets/img/icon_7.png" />
+        <div class="tester-content">
+          检定人员：<span>{{ item.name }}</span>
+        </div>
+      </ol>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ["testerList"],
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.unusual {
+  height: 30rem !important;
+}
+.row {
+  height: 80% !important;
+  overflow: auto;
+}
+.title {
+  padding: 1.2rem 0 1rem 4rem !important;
+  margin-left: 2rem !important;
+}
+.icon-avatar {
+  display: inline;
+  margin-right: 1rem;
+  width: 5rem;
+}
+.tester-content {
+  padding-left: 1rem;
+  display: inline-flex;
+  height: 3.7rem;
+  width: 22rem;
+  background-image: url("../../../assets/img/icon_8.png");
+}
+::-webkit-scrollbar {
+  display: none;
+}
+</style>
