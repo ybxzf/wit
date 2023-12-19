f51ad90739fc2383c48dd6575ab2d98c25df9426
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-12-19 14:34:27 +0800

    Update index.vue

--- src/page/facilities/index.vue
+++ src/page/facilities/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="indexBox">
     <el-row>
-      <el-col :span="6" style="border: 1px solid red;height: 77rem ;padding: 1rem 0 1rem 1rem;">
+      <el-col
+        :span="6"
+        style="border: 1px solid red; height: 77rem; padding: 1rem 0 1rem 1rem"
+      >
         <el-row>
           <el-col :span="24">
             <device-inform :deviceList="this.deviceList" />
@@ -18,7 +21,9 @@
         </el-row>
       </el-col>
       <el-col :span="18">
-        <electric-meter-watch :processWatchList="this.processWatchList" />
+        <!-- <electric-meter-watch :processWatchList="this.processWatchList" /> -->
+        <!-- <normal-meter-watch /> -->
+        <transformer-watch />
       </el-col>
     </el-row>
   </div>
@@ -30,10 +35,20 @@ import tester from "./components/tester.vue";
 import envWatch from "./components/envWatch.vue";
 import realTimeData from "./components/realTimeData.vue";
 import electricMeterWatch from "./components/electricMeterWatch.vue";
+import normalMeterWatch from "./components/normalMeterWatch.vue";
+import transformerWatch from "./components/transformerWatch.vue";
 import { electricMeterData } from "./virtualData.js";
 
 export default {
-  components: { deviceInform, tester, envWatch, realTimeData, electricMeterWatch }, //注册组件
+  components: {
+    deviceInform,
+    tester,
+    envWatch,
+    realTimeData,
+    electricMeterWatch,
+    normalMeterWatch,
+    transformerWatch,
+  }, //注册组件
   name: "facilities",
   data() {
     return {
@@ -48,8 +63,8 @@ export default {
       ],
       testerList: [
         {
-          "personnelName": "江正涛",
-          "post": "电能，电阻"
+          personnelName: "江正涛",
+          post: "电能，电阻",
         },
       ],
       envList: {
@@ -58,42 +73,42 @@ export default {
         illumination: "685lux",
         magneticField: "10mg/L",
       },
-      // realTimeList: {
-      //   "equipName": "直流电能表检定装置",
-      //   "phia": "0.00",
-      //   "phib": "240.00",
-      //   "phic": "120.00",
-      //   "checker": "江正涛",
-      //   "Ua": "219.99",
-      //   "Ub": "220.01",
-      //   "Uc": "220.10",
-      //   "La": "5.01",
-      //   "equipType": "电能表",
-      //   "Lb": "4.99",
-      //   "Lc": "4.98",
-      //   "equipNo": "2304##002##TH001",
-      //   "projectName": "Q+ 合元 0.8C 1.0Ib 基本误差"
-      // },
       realTimeList: {
-        "phase": "123",
-        "equipName": "三相谐波表检定装置",
-        "lightLoad": "123",
-        "rating": "123",
-        "accuracy": "123",
-        "oneCurrent": "123",
-        "updateTime": 1702650566000,
-        "checker": "江正涛",
-        "params": {},
-        "booster": "123",
-        "twoCurrent": "123",
-        "equipType": "互感器",
-        "taskNo": "1202002",
-        "equipNo": "2304##002##WSD001",
-        "id": 1,
-        "resistor": "123",
-        "projectName": "Q+ 合元 0.8C 1.0Ib 基本误差",
-        "powerFactor": "123"
+        equipName: "直流电能表检定装置",
+        phia: "0.00",
+        phib: "240.00",
+        phic: "120.00",
+        checker: "江正涛",
+        Ua: "219.99",
+        Ub: "220.01",
+        Uc: "220.10",
+        La: "5.01",
+        equipType: "电能表",
+        Lb: "4.99",
+        Lc: "4.98",
+        equipNo: "2304##002##TH001",
+        projectName: "Q+ 合元 0.8C 1.0Ib 基本误差",
       },
+      // realTimeList: {
+      //   phase: "123",
+      //   equipName: "三相谐波表检定装置",
+      //   lightLoad: "123",
+      //   rating: "123",
+      //   accuracy: "123",
+      //   oneCurrent: "123",
+      //   updateTime: 1702650566000,
+      //   checker: "江正涛",
+      //   params: {},
+      //   booster: "123",
+      //   twoCurrent: "123",
+      //   equipType: "互感器",
+      //   taskNo: "1202002",
+      //   equipNo: "2304##002##WSD001",
+      //   id: 1,
+      //   resistor: "123",
+      //   projectName: "Q+ 合元 0.8C 1.0Ib 基本误差",
+      //   powerFactor: "123",
+      // },
       processWatchList: [],
     };
   },
