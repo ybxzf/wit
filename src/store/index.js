d6e08e4be2e7a3c71d286d5793f5911a607fb7e9
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-10 15:53:37 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,17 @@
 import { defineStore } from 'pinia' //引入
 
-const useDeviceStore = defineStore('deviceStatus', {
+const useDeviceStore = defineStore('device-status', {
     // arrow function recommended for full type inference
     state: () => {
         return {
-            status: '',
+            status: '空闲',
         }
     },
     // 相当于 vue 中的 methods 方法
     actions: {
         updateStatus(newStatus) {
-            this.status = newStatus;
+            this.status = newStatus.trim() || '空闲';
+            // console.log('调用状态方法更新',this.status);
         }
     }
 })
